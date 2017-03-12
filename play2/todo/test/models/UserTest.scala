547d356399c28b0d9044348aeb6f7609abf98180
Yoshihito Hirakawa <hammer.and.heart.daphne@gmail.com>
2017-03-12 10:59:24 +0900

    ユーザの単体テストを調整、renameで関心の無い項目(=id)をモックに変更

--- play2/todo/test/models/UserTest.scala
+++ play2/todo/test/models/UserTest.scala
@@ -4,6 +4,8 @@ import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play._
 
 class UserTest extends PlaySpec with MockitoSugar {
+  def mockUser(): User = User(mock[UserId], "")
+
   "User#rename" should {
     "has same user id" in {
       val srcUser = User(UserId(1), "Taro")
@@ -14,7 +16,7 @@ class UserTest extends PlaySpec with MockitoSugar {
     }
 
     "be immutable" in {
-      val srcUser = User(UserId(1), "Taro")
+      val srcUser = mockUser().copy(name = "Taro")
       val renamedUser = srcUser.rename("Jiro")
 
       srcUser.name mustBe "Taro"
